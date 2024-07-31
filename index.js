6577f52cf7995647cffa3e85e2204d62cb2c7a50
NicoleScott-web <nicolemilyscott@gmail.com>
2024-07-31 19:44:11 +1200

    Array

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+let Dogs = ['Poodle', 'Husky', 'Schnauzer'];
+console.log(Dogs[2]); // 'Poodle'
+console.log(Dogs[0]); // 'Husky'
+console.log(Dogs[1]); // 'Schnauzer'
+console.log (Dogs)
+Dogs.unshift('Kelpie');
+console.log(Dogs); // [ 'Kelpie', 'Poodle', 'Husky', 'Schnauzer' ]
+Dogs.push('Poodle');
+console.log(Dogs);
+Dogs[0] = 'Husky';
+console.log(Dogs); // [ 'Kelpie', 'Poodle', 'Husky', 'Schnauzer', 'Poodle' ]
+let firstDogs = Dogs.shift();
+console.log(Dogs)
+
+
+
+
